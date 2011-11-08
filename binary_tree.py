f01713e1b6faa4dc0e927a23002b88295af93260
Yoshinori Fukushima <fukushimayoshinori@fukushima-yoshinori-no-MacBook-Pro.local>
2011-11-09 03:05:43 +0900

    binary_tree first commit
    I tried to code binary_tree in python. This program is under construction.

---
+++ binary_tree.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+    
+class Node:
+    def __init__(self,x):
+        self.data=x
+        self.left=None
+        self.right=None
+
+def search(node,x):
+    while node:
+        if node.data==x:
+            return True
+            
+        if x<node.data:
+            node=node.left
+
+        else:
+            node=node.right
+        
+    return False
+
+def insert(node,x):
+    if node is None:
+        return Node(x)
+    elif x==node.data:
+        return node
+    elif x<node.data:
+        node.left=insert(node.left,x)
+    else:
+        node.right=insert(node.right,x)
+    
+    return node

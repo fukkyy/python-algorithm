9decd090d81d8302b0475886be4d00e1f87986f2
Yoshinori Fukushima <fukushimayoshinori@fukushima-yoshinori-no-MacBook-Pro.local>
2012-02-03 03:52:24 +0900

    delete

--- test.py
+++
@@ -1,9 +0,0 @@
-import socialStream
-
-social=socialStream.socialStream()
-
-url_list=social.userTimelineToUrl(access_token_key='94355673-WphJCUYWbjzQ0UisZJX2I2LaQuwlbK3fo7CRDEm3A',access_token_secret='Obq4CA7dRcbFXJl6KO0EHFYQbx6SuSIg9rciBN6Rs')
-
-real_url_list=social.getRealUrl(url_list)
-
-print real_url_list

2278af4185cef030ae980d8f123e2f12713ef7a8
Yoshinori Fukushima <fukushimayoshinori@fukushima-yoshinori-no-MacBook-Pro.local>
2012-02-13 04:42:38 +0900

    bag modified

--- queue.py
+++ queue.py
@@ -1,13 +1,37 @@
+#! /usr/local/bin/python
 # -*- coding:utf-8 -*-
 
-class queue(data=[]):
+class queue():
     
-    self.data=data
+    def __init__(self,data=[]):
+        self.data=data
 
-    def enqueue(self,string):
-        self.data.append(string)
+    def queue_empty(self):
+        if len(self.data)==0:
+            return True
+        else:
+            return False
+
+    def enqueue(self,x):
+        self.data.append(x)
 
     def dequeue(self):
-        result=data[0]
-        self.data=self.data[1:]
-        return result
+        if self.queue_empty():
+            print 'queue is empty'
+        else:
+            result=self.data[0]
+            self.data=self.data[1:]
+            return result
+
+if __name__=='__main__':
+    q=queue()
+    q.enqueue(2)
+    print q.data
+    q.enqueue(3)
+    print q.data
+    q.dequeue()
+    print q.data
+    q.dequeue()
+    print q.data
+    q.dequeue()
+    print q.data

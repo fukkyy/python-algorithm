e74b986ae4dd46bda9e5a36cf142a2f80a9fb43d
Yoshinori Fukushima <fukushimayoshinori@fukushima-yoshinori-no-MacBook-Pro.local>
2012-02-13 04:15:50 +0900

    change

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,23 +1,18 @@
+#! /usr/local/bin/python
 # -*- coding:utf-8 -*-
 
-def bubbleSort(array):
-    n=len(array)
-    for i in range(0,n-1):
-        j=n-1
-        while j>0:
-            if array[j]<array[j-1]:
-                n1=array[j]
-                n2=array[j-1]
-                array[j]=n2
-                array[j-1]=n1
-            j-=1
-            if j==i:
-                break
-    return array
+def bubble_sort(A):
+    n=len(A)
+    for i in range(n):
+        for j in range(n-1-i):
+            if A[n-1-j]<A[n-2-j]:
+                A[n-1-j],A[n-2-j]=A[n-2-j],A[n-1-j]
+    return A
+
 
 if __name__=='__main__':
-    array=[18,35,76,23,49,42,31,12]
+    A=[18,35,76,23,49,42,31,12]
     print 'before sort:'
-    print array
+    print A
     print 'after sort:'
-    print bubbleSort(array)
+    print bubble_sort(A)

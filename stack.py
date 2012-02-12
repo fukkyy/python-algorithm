d69b0d431875d83fefb51c5fe25aeb131148153c
Yoshinori Fukushima <fukushimayoshinori@fukushima-yoshinori-no-MacBook-Pro.local>
2012-02-13 04:36:01 +0900

    change

--- stack.py
+++ stack.py
@@ -1,14 +1,39 @@
+#! /usr/local/bin/python
 # -*- coding:utf-8 -*-
 
-class stack(data=[]):
 
-    self.data=data
+class stack:
 
-    def push(self,input_data):
-        self.data.append(input_data)
+    def __init__(self,data=[]):
+        self.data=data
+    
+    def stack_empty(self):
+        if len(self.data)==0:
+            return True
+        else:
+            return False
+
+    def push(self,x):
+        self.data.append(x)
 
     def pop(self):
-        num=len(self.data)-1
-        result=self.data[num]
-        self.data=self.data[0:num]
-        return result
+        if self.stack_empty():
+            print 'stack is empty'
+        else:
+            num=len(self.data)-1
+            result=self.data[num]
+            self.data=self.data[0:num]
+            return result
+
+if __name__=='__main__':
+    s=stack()
+    s.push(2)
+    print s.data
+    s.push(3)
+    print s.data
+    s.pop()
+    print s.data
+    s.pop()
+    print s.data
+    s.pop()
+    print s.data

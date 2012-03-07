633c65f28d59f1de91940ca9263c5543b6c76a0f
Yoshinori Fukushima <fukushimayoshinori@fukushima-yoshinori-no-MacBook-Pro.local>
2012-03-07 19:46:03 +0900

    sosu.py first commit

---
+++ sosu.py
@@ -0,0 +1,28 @@
+#! /usr/local/bin/python
+# -*- coding:utf-8 -*-
+
+def check_sosu(num):
+    flag=0
+    #素数かどうかの判定
+    i=2
+    while True:
+        if i>=num:
+            break
+        if num%i==0:
+            flag=1
+            break
+        else:
+            i+=1
+    if flag==0:
+        return True
+    else:
+        return False
+
+def count_sosu(default=1000):
+    import time
+    num=2
+    for i in range(default):
+        if check_sosu(num):
+            print num
+            time.sleep(0.5)
+        num+=1
